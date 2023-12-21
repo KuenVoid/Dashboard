298a270b61b6865906835f068c71dfa3fb7a3fb7
KuenVoid <131005034+KuenVoid@users.noreply.github.com>
2023-12-21 20:09:31 +0800

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,33 +1,67 @@
+import Editicon from './assets/Edit.svg'
+import { useState, useEffect } from 'react';
+
 const Home = (styles) => {
-    var todo1 = localStorage.getItem('todo1');
-    var todo2 = localStorage.getItem('todo2');
-    var todo3 = localStorage.getItem('todo3');
-    var todo4 = localStorage.getItem('todo4');
+    const [editstyle, seteditstyle] = useState('none');
+    const [inputstyle1, setinputstyle1] = useState('none');
+    const [inputstyle2, setinputstyle2] = useState('none');
+    const [inputstyle3, setinputstyle3] = useState('none');
+    const [inputstyle4, setinputstyle4] = useState('none');
+    var todo1data = localStorage.getItem('todo1') + " -" + localStorage.getItem('dl1');
+    var todo2data = localStorage.getItem('todo2') + " -" + localStorage.getItem('dl2');
+    var todo3data = localStorage.getItem('todo3') + " -" + localStorage.getItem('dl3');
+    var todo4data = localStorage.getItem('todo4') + " -" + localStorage.getItem('dl4');
+    if(!localStorage.getItem('todo1') || !localStorage.getItem('dl1') || localStorage.getItem('dl1') == ' ') {
+        todo1data = '';
+    }
+    if(!localStorage.getItem('todo2') || !localStorage.getItem('dl2') || localStorage.getItem('dl2') == ' ') {
+        todo2data = '';
+    }
+    if(!localStorage.getItem('todo3') || !localStorage.getItem('dl3') || localStorage.getItem('dl3') == ' ') {
+        todo3data = '';
+    }
+    if(!localStorage.getItem('todo4') || !localStorage.getItem('dl4') || localStorage.getItem('dl4') == ' ') {
+        todo4data = '';
+    }
 
-    if(!todo1 || todo1 == ' '){
-        todo1 = 'Things to do:';
+    const change1 = () => {
+        if(inputstyle1 == 'none'){
+            setinputstyle1('block');
+        } else {
+            setinputstyle1('none');
+        }
     }
-    if(!todo2 || todo22 == ' '){
-        todo2 = 'Things to do:';
+    const change2 = () => {
+        if(inputstyle2 == 'none'){
+            setinputstyle2('block');
+        } else {
+            setinputstyle2('none');
+        }
     }
-    if(!todo3 || todo3 == ' '){
-        todo3 = 'Things to do:';
+    const change3 = () => {
+        if(inputstyle3 == 'none'){
+            setinputstyle3('block');
+        } else {
+            setinputstyle3('none');
+        }
     }
-    if(!todo4 || todo4 == ' '){
-        todo4 = 'Things to do:';
+    const change4 = () => {
+        if(inputstyle4 == 'none'){
+            setinputstyle4('block');
+        } else {
+            setinputstyle4('none');
+        }
     }
-
-    const todolog1 = (tododata) => {
-        localStorage.setItem('todo1', tododata.target.value);
+    const changeeditstyle = () => {
+        if(editstyle == 'none'){
+            seteditstyle('block');
+        } else {
+            seteditstyle('none');
+        }
     }
-    const todolog2 = (tododata) => {
-        localStorage.setItem('todo2', tododata.target.value);
-    }
-    const todolog3 = (tododata) => {
-        localStorage.setItem('todo3', tododata.target.value);
-    }
-    const todolog4 = (tododata) => {
-        localStorage.setItem('todo4', tododata.target.value);
+    const savetododata = e => {
+        const id = e.target.id;
+        localStorage.setItem(id, e.target.value);
     }
 
     return (
@@ -40,10 +74,34 @@ const Home = (styles) => {
             </div>
             <div className="todo">
                 <h2>To-do list</h2>
-                <input type="text" placeholder={todo1} id="todo1" onChange={ todolog1 }/>
-                <input type="text" placeholder={todo2} id="todo2" onChange={ todolog2 }/>
-                <input type="text" placeholder={todo3} id="todo3" onChange={ todolog3 }/>
-                <input type="text" placeholder={todo4} id="todo4" onChange={ todolog4 }/>
+                <img src={ Editicon } alt="editicon" id='editable' onClick={ changeeditstyle }/>
+                <h2 id='todolists1'> { todo1data }</h2>
+                <h2 id='todolists2'> { todo2data }</h2>
+                <h2 id='todolists3'> { todo3data }</h2>
+                <h2 id='todolists4'> { todo4data }</h2>
+            </div>
+            <div className='edittodo' style={ {display:editstyle} }>
+                <h2>Edit your Todo list</h2>
+                <div className='container'>
+                    <h2 onClick={ change1 }>To-do 1</h2>
+                    <input type="text" placeholder='To-do thing:' style={ {display: inputstyle1} } id='todo1' onChange={ savetododata }/>
+                    <input type="text" placeholder='Deadline:' style={ {display: inputstyle1} } id='dl1' onChange={ savetododata }/>
+                </div>
+                <div className='container'>
+                    <h2 onClick={ change2 }>To-do 2</h2>
+                    <input type="text" placeholder='To-do thing:' style={ {display: inputstyle2} } id='todo2' onChange={ savetododata }/>
+                    <input type="text" placeholder='Deadline:' style={ {display: inputstyle2} } id='dl2' onChange={ savetododata }/>
+                </div>
+                <div className='container'>
+                    <h2 onClick={ change3 }>To-do 3</h2>
+                    <input type="text" placeholder='To-do thing:' style={ {display: inputstyle3} } id='todo3' onChange={ savetododata }/>
+                    <input type="text" placeholder='Deadline:' style={ {display: inputstyle3} } id='dl3' onChange={ savetododata }/>
+                </div>
+                <div className='container'>
+                    <h2 onClick={ change4 }>To-do 4</h2>
+                    <input type="text" placeholder='To-do thing:' style={ {display: inputstyle4} } id='todo4' onChange={ savetododata }/>
+                    <input type="text" placeholder='Deadline:' style={ {display: inputstyle4} } id='dl4' onChange={ savetododata }/>
+                </div>
             </div>
         </div>
     );
