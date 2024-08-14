8e6fa61967b5e86d836daba8506b4f409217e366
KuenVoid <131005034+KuenVoid@users.noreply.github.com>
2024-08-14 21:41:13 +0800

    Update Tool.jsx

--- src/Tool.jsx
+++ src/Tool.jsx
@@ -33,20 +33,32 @@ const Tools = (styles) => {
         var itemlength = Number(itemarray.length);
         setresult(itemarray[Math.floor(Math.random() * itemlength)]);
     }
+    const progresschange = (e) => {
+        localStorage.setItem(e.target.id, e.target.value);
+    }
+    const percentagechange = (e) => {
+        if (Number(e.target.value) > 100) {
+            localStorage.setItem(e.target.id, 100);
+        } else if (Number(e.target.value) < 0) {
+            localStorage.setItem(e.target.id, 0);
+        } else {
+            localStorage.setItem(e.target.id, e.target.value);
+        }
+    }
 
     return (
         <div className='pages' {...styles}>
-            <div className="luckydraw">
+            <div className="luckydraw blocks">
                 <h2>Lucky draw</h2>
                 <h2 className='add' onClick={ changedrawdisplay }>+</h2>
                 <h2>Rewards: <br/> {items}</h2>
                 <h2>{result}</h2>
                 <input type="button" value="Spin" id='spinbut' onClick={ spin }/>
                 <div className='drawinput' style={ {display:drawinput} }>
-                    <input type="text" placeholder='Items1, Items2 ...' onChange={setitems}/>
+                    <input type="text" placeholder='Items1, Items2, ...' onChange={setitems}/>
                 </div>
             </div>
-            <div className="log">
+            <div className="log blocks">
                 <h2>Items log:</h2>
                 <h2 className='add' onClick={ changelogdisplay }>+</h2>
                 <div style={ {display:logdisplay} } className="inputcontainer">
@@ -54,6 +66,26 @@ const Tools = (styles) => {
                     <input type="text" placeholder='Logs' id='log2'/>
                     <input type="text" placeholder='Logs' id='log3'/>
                     <input type="text" placeholder='Logs' id='log4'/>
+                </div>
+            </div>
+            <div className="activitymonitor blocks">
+                <h2>Activity Monitor</h2>
+                <h2>Enter your progress:</h2>
+                <div className='progressinputs'>
+                    <input type="text" placeholder='Progess name' id='progress1' onChange={ progresschange }/>
+                    <input type="number" min={0} max={100} id='percentage1' placeholder='%' onChange={ percentagechange }/>
+                </div>
+                <div className='progressinputs'>
+                    <input type="text" placeholder='Progess name' id='progress2' onChange={ progresschange }/>
+                    <input type="number" min={0} max={100} id='percentage2' placeholder='%' onChange={ percentagechange }/>
+                </div>
+                <div className='progressinputs'>
+                    <input type="text" placeholder='Progess name' id='progress3' onChange={ progresschange }/>
+                    <input type="number" min={0} max={100} id='percentage3' placeholder='%' onChange={ percentagechange }/>
+                </div>
+                <div className='progressinputs'>
+                    <input type="text" placeholder='Progess name' id='progress4' onChange={ progresschange }/>
+                    <input type="number" min={0} max={100} id='percentage4' placeholder='%' onChange={ percentagechange }/>
                 </div>
             </div>
         </div>
