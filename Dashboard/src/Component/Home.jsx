8002d90e80e47232b924559ee9c0998eb35e87c4
KuenVoid <kuen.kwong666@gmail.com>
2026-05-24 10:21:20 +0800

    Added Append Feature

--- Dashboard/src/Component/Home.jsx
+++ Dashboard/src/Component/Home.jsx
@@ -14,27 +14,29 @@ export default function Home() {
   const [editPerc, setEditPerc] = useState(0);
   const [editTitle, setEditTitle] = useState('');
   const [editStatus, setEditStatus] = useState('');
-  const [createModalOpen, setcreateModalOpen] = useState(false);
+  const [createModal, setcreateModal] = useState({isOpen: false});
+  const [newPerc, setNewPerc] = useState(0);
+  const [newTitle, setNewTitle] = useState('New Goal');
+  const [newStatus, setNewStatus] = useState('Stable');
 
   const [GoalMetrics, setGoalMetrics] = useState(() => {
     const savedData = localStorage.getItem("dashboard_Goal_metrics");
     
-    return savedData ? JSON.parse(savedData) : [
-      { id: "1", title: "Project Alpha", percentage: 85, status: "good" },
-      { id: "2", title: "Core Infrastructure", percentage: 60, status: "good" },
-      { id: "3", title: "Beta Program", percentage: 45, status: "stable" }
-    ];
+    return savedData ? JSON.parse(savedData) : [];
   });
-  const largest_id = GoalMetrics.length > 0 ? GoalMetrics.at(-1).id : "0";
 
   // Start Right Click Function
   const handleRightClick = (e, metricObj) => {
     e.stopPropagation();
     e.preventDefault();
+    const menuHeight = (metricObj)? 165: 95;
+    const viewportHeight = window.innerHeight;
+    const y_cord = (e.clientY + menuHeight > viewportHeight)? e.clientY - menuHeight: e.clientY;
+
     setMenuSettings({
       visible: true,
       x: e.clientX,
-      y: e.clientY,
+      y: y_cord,
       targetCategory: metricObj
     });
   };
@@ -56,7 +58,7 @@ export default function Home() {
   };
 
   const ModalCreateOpen = () => {
-    setCreateModalOpen(true); 
+    setcreateModal({isOpen: true}); 
     setMenuSettings({ ...menuSettings, visible: false });
   };
 
@@ -73,6 +75,19 @@ export default function Home() {
     saveGoal(updatedList);
     setEditModal({ isOpen: false, targetMetric: null });
   }
+  const AppendGoal = () => {
+    const newgoal = {
+      id: `p${Date.now()}`,
+      title: newTitle,
+      percentage: newPerc,
+      status: newStatus
+    }
+    const updatedList = [...GoalMetrics, newgoal];
+    setGoalMetrics(updatedList);
+    setcreateModal({isOpen: false});
+    saveGoal(updatedList);
+  }
+  
   return (
     <div className="home-container" onClick={(e) => closeMenu(e)}>
       <header className="home-header"><h1 className="welcome-title">Home</h1></header>
@@ -87,7 +102,7 @@ export default function Home() {
         {/* This is the summary block */}
         <div className="dash-card summary-card">
           <div className="summary-stat">
-            <h2>Placeholder</h2>
+            <h2>Today's Calendar</h2>
           </div>
         </div>
         {/* This is the goal session */}
@@ -114,6 +129,11 @@ export default function Home() {
             </div>
           );
         })}
+          </div>
+        </div>
+        <div className="dash-card todo-card">
+          <div className="todo-stat">
+            <h2>Important Todo</h2>
           </div>
         </div>
       </div>
@@ -164,6 +184,40 @@ export default function Home() {
             <div className="modal-actions">
               <button onClick={() => setEditModal({ ...editModal, isOpen: false })}>Cancel</button>
               <button className="save-btn" onClick={changeGoal}>Save Changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )}
+    {createModal.isOpen && (
+      <div className="modal-backdrop">
+        <div className="edit-modal-card">
+          <div className="modal-header">
+            <h2>Append Goal</h2>
+            <button onClick={() => setcreateModal({ ...editModal, isOpen: false })}>✕</button>
+          </div>
+          <div className="modal-body">
+            <input type="text" value={newTitle}
+            onChange={(e) => setNewTitle(String(e.target.value))}/>
+            <select id="StatusChoice" className="Multiple-Choices" value={newStatus}
+            onChange={(e) => setNewStatus(String(e.target.value))}>
+              <option value="Good">Good</option>
+              <option value="Stable">Stable</option>
+              <option value="Bad">Bad</option>
+            </select>
+            
+            <label>New Percentage ({newPerc}%)</label>
+            <input 
+              type="range" 
+              min="0" 
+              max="100" 
+              value={newPerc} // Bind the state value
+              onChange={(e) => setNewPerc(Number(e.target.value))} // Update state on change
+            />
+            
+            <div className="modal-actions">
+              <button onClick={() => setcreateModal({ ...editModal, isOpen: false })}>Cancel</button>
+              <button className="save-btn" onClick={AppendGoal}>Save Changes</button>
             </div>
           </div>
         </div>
---
+++ Dashboard/src/Component/Todo.jsx
@@ -0,0 +1,9 @@
+import react from "React";
+
+export default function Todo(){
+
+
+    return (
+        <div className="Todo"></div>
+    )
+}
